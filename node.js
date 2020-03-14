3a50c1303cb67de7348a021d6ce3aa56e35fc92d
hokhanhduycs <hokhanhduycs@gmail.com>
2020-03-14 09:12:48 +0700

    add project

---
+++ node.js
@@ -0,0 +1,17 @@
+console.log("HelloWord");
+
+const a = 1; // never change later
+let b = 2; // change change
+
+//Trick: Always use 'const'
+//When you need to change the value then yon can modify 'const' -> 'let'
+//
+
+const c = a + b;
+console.log('Result: ', c);
+console.log('Result: ' + c);
+console.log('Result should be : ${c}');
+
+console.warn('Hi all');
+console.error('My Error');
+
